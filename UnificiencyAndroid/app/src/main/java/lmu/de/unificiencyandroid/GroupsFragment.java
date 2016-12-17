ca02c2cff50ae6ddefa0d2ab206e085d16c42751
Robert Müller <robertMueller@Roberts-MacBook-Pro.local>
2016-12-17 15:40:54 +0100

    small adapter for GroupsFragment

--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/GroupsFragment.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/GroupsFragment.java
@@ -3,7 +3,10 @@ package lmu.de.unificiencyandroid;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +21,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class GroupsFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,21 +68,29 @@ public class GroupsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+        View x = inflater.inflate(R.layout.fragment_groups, container, false);
+        mRecyclerView = (RecyclerView) x.findViewById(R.id.groups_recycler_view);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        return x;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+
+
+
+
 
     @Override
     public void onAttach(Context context) {
