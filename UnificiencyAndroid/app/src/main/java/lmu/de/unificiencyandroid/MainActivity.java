9bfaaa7625c20bfb2627ebf457f001bb27844512
zhenhaoli <zhenhao.li@outlook.com>
2016-12-18 12:05:15 +0100

    refactored menu items selection

--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/MainActivity.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/MainActivity.java
@@ -64,43 +64,50 @@ public class MainActivity extends AppCompatActivity{
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 mDrawerLayout.closeDrawers();
 
-                if (menuItem.getItemId() == R.id.nav_item_buildings) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new BuildingsTab()).commit();
-                    getSupportActionBar().setTitle(getString(R.string.nav_item_buildings));
+                switch (menuItem.getItemId()){
+                    case R.id.nav_item_buildings: {
+                        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.containerView,new BuildingsTab()).commit();
+                        getSupportActionBar().setTitle(getString(R.string.nav_item_buildings));
+                        break;
+                    }
+                    case R.id.nav_item_groups: {
+                        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.containerView,new GroupsFragment()).commit();
+                        getSupportActionBar().setTitle(getString(R.string.nav_item_groups));
+                        break;
+                    }
+
+                    case R.id.nav_item_notes: {
+                        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.containerView,new NotesTab()).commit();
+                        getSupportActionBar().setTitle(getString(R.string.nav_item_notes));
+                        break;
+                    }
+
+                    case R.id.nav_item_setting: {
+                        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.containerView,new SentFragment()).commit();
+                        getSupportActionBar().setTitle(getString(R.string.nav_item_setting));
+                        break;
+                    }
+
+                    case R.id.nav_item_account: {
+                        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.containerView,new SentFragment()).commit();
+                        getSupportActionBar().setTitle(getString(R.string.nav_item_account));
+                        break;
+                    }
+
+                    case R.id.nav_item_logout: {
+                        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(loginIntent);
+                        finish();
+                        break;
+                    }
+
                 }
 
-                if (menuItem.getItemId() == R.id.nav_item_groups) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new GroupsFragment()).commit();
-                    getSupportActionBar().setTitle(getString(R.string.nav_item_groups));
-                }
-
-                if (menuItem.getItemId() == R.id.nav_item_notes) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new NotesTab()).commit();
-                    getSupportActionBar().setTitle(getString(R.string.nav_item_notes));
-
-                }
-
-                if (menuItem.getItemId() == R.id.nav_item_setting) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new SentFragment()).commit();
-                    getSupportActionBar().setTitle(getString(R.string.nav_item_setting));
-                }
-
-                if (menuItem.getItemId() == R.id.nav_item_account) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new SentFragment()).commit();
-                    getSupportActionBar().setTitle(getString(R.string.nav_item_account));
-                }
-
-                if (menuItem.getItemId() == R.id.nav_item_logout) {
-                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(loginIntent);
-                    finish();
-                }
-                
                 return false;
             }
 
@@ -112,7 +119,7 @@ public class MainActivity extends AppCompatActivity{
 
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.app_name,
-                R.string.app_name);
+            R.string.app_name);
 
         mDrawerLayout.addDrawerListener(mDrawerToggle);
 
@@ -123,15 +130,15 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this)
-                .setMessage("Unificieny beenden?")
-                .setCancelable(false)
-                .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        MainActivity.this.finish();
-                    }
-                })
-                .setNegativeButton("Abbrechen", null)
-                .show();
+            .setMessage("Unificieny beenden?")
+            .setCancelable(false)
+            .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    MainActivity.this.finish();
+                }
+            })
+            .setNegativeButton("Abbrechen", null)
+            .show();
     }
 
 
