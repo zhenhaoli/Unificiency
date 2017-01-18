bcff54081caec592daf28adf8f8d03c4366fd6a7
Jindong <ostdong@gmail.com>
2017-01-18 21:31:51 +0100

    backbtn funktionieren

--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/notes/NewNote.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/notes/NewNote.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -52,13 +53,18 @@ public class NewNote extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
-
-
     }
+    /* restore back button functionality*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home: {
+                onBackPressed();
+                return true;
+            }
+            default:{return super.onOptionsItemSelected(item);}
+
+        }
+    }
+
 }
