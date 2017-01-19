abb95fd3d885a6ab5cda5023bde61167145918e0
zhenhaoli <zhenhao.li@outlook.com>
2017-01-19 10:34:41 +0100

    firebase cloud messaging integration

---
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/network/FCMInstanceIDService.java
@@ -0,0 +1,30 @@
+package lmu.de.unificiencyandroid.network;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static com.google.android.gms.internal.zzs.TAG;
+
+
+public class FCMInstanceIDService extends FirebaseInstanceIdService {
+  @Override
+  public void onTokenRefresh() {
+    // Get updated InstanceID token.
+    String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+    Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+
+    sendRegistrationToServer(refreshedToken);
+  }
+
+  // TODO: Implement this method to send any registration to your app's servers.
+  public void sendRegistrationToServer(String token){
+
+  }
+}
+
+
+
+
