01d0549357c4dd6fd9dcf55cac2dcff91b2475c0
zhenhaoli <zhenhao.li@outlook.com>
2017-01-19 18:39:48 +0100

    mock notes in groups

--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/setting/SettingGroups.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/setting/SettingGroups.java
@@ -1,11 +1,10 @@
 package lmu.de.unificiencyandroid.components.setting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import lmu.de.unificiencyandroid.R;
-import lmu.de.unificiencyandroid.components.notes.NotesOfGroup;
 
 public class SettingGroups extends AppCompatActivity {
 
@@ -17,7 +16,7 @@ public class SettingGroups extends AppCompatActivity {
         setContentView(R.layout.setting_groups);
 
         listView=(ListView) findViewById(R.id.settinggroup_list);
-        SettingGroupsAdapter adapter= new SettingGroupsAdapter(this, android.R.layout.simple_list_item_1, NotesOfGroup.GroupNotes);
+        SettingGroupsAdapter adapter= new SettingGroupsAdapter(this, android.R.layout.simple_list_item_1, null);
         listView.setAdapter(adapter);
 
     }
