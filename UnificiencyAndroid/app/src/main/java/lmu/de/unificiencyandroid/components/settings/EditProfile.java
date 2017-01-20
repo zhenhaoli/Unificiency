05dd4f881b509bb85894924f3d8ea3b47981bb62
zhenhaoli <zhenhao.li@outlook.com>
2017-01-20 17:21:43 +0100

    client added camera test fragment and server added upload multi files service

--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/settings/EditProfile.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/settings/EditProfile.java
@@ -1,11 +1,12 @@
 package lmu.de.unificiencyandroid.components.settings;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
@@ -21,148 +22,152 @@ import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 import lmu.de.unificiencyandroid.R;
-import lmu.de.unificiencyandroid.components.login.LoginActivity;
 import lmu.de.unificiencyandroid.network.PythonAPIClient;
 import lmu.de.unificiencyandroid.network.UnificiencyClient;
 import lmu.de.unificiencyandroid.utils.SharedPref;
 
-import static java.security.AccessController.getContext;
-
 public class EditProfile extends AppCompatActivity {
 
-    Button save;
+  Button save;
 
-    TextInputEditText nickName_editor, majorName_editor,text_email;
+  TextInputEditText nickName_editor, majorName_editor,text_email;
 
-    public void getUserInfo() {
-        String authToken =  SharedPref.getDefaults("authTokenPython", getApplicationContext());
+  public void getUserInfo() {
+    String authToken =  SharedPref.getDefaults("authTokenPython", getApplicationContext());
 
-        final RequestParams params = new RequestParams();
+    final RequestParams params = new RequestParams();
 
-        UnificiencyClient client = new PythonAPIClient();
+    UnificiencyClient client = new PythonAPIClient();
 
-        client.addHeader("Authorization", authToken);
-        client.get("users/", params, new JsonHttpResponseHandler() {
+    client.addHeader("Authorization", authToken);
+    client.get("users/", params, new JsonHttpResponseHandler() {
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                try {
+      @Override
+      public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        try {
 
-                    String nickName = response.getString("username");
-                    String majorName = response.getString("major");
-                    String email = response.getString("email");
+          String nickName = response.getString("username");
+          String majorName = response.getString("major");
+          String email = response.getString("email");
 
-                    nickName_editor.setText(nickName);
-                    majorName_editor.setText(majorName);
-                    text_email.setText(email);
-
-
-                } catch (Exception e) {
-                    Log.e("getUserInfo", e.toString());
-                }
-            }
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String errmsg, Throwable throwable) {
-                Log.e("getUserInfo", errmsg);
-
-            }
-        });
-    }
-
-    public void setUserInfo() {
-
-        String nickName = this.nickName_editor.getText().toString();
-        String majorName = this.majorName_editor.getText().toString();
-        String email = this.text_email.getText().toString();
-
-        String authToken =  SharedPref.getDefaults("authTokenPython", getApplicationContext());
-
-        final RequestParams params = new RequestParams();
-        params.put("username",nickName);
-        params.put("email",email);
-        params.put("major",majorName);
-        params.setUseJsonStreamer(true);
-
-        UnificiencyClient client = new PythonAPIClient();
-
-        client.addHeader("Authorization", authToken);
-        client.put("users/", params, new JsonHttpResponseHandler() {
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                // Pull out the first event on the public timeline
-                try {
-                    SuperActivityToast.cancelAllSuperToasts();
-                    SuperActivityToast.create(getApplicationContext(), new Style(), Style.TYPE_STANDARD)
-                            .setText(response.toString())
-                            .setDuration(Style.DURATION_LONG)
-                            .setFrame(Style.FRAME_KITKAT)
-                            .setColor(ResourcesCompat.getColor(getResources(), R.color.green_400, null))
-                            .setAnimations(Style.ANIMATIONS_SCALE)
-                            .show();
-                    finish();
-
-                } catch (Exception e) {
-                    Log.e("modifyUserInfo", "success");
-                }
-            }
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                Log.e("modifyUserInfo", errorResponse.toString());
-                String errmsg = null;
-                try {
-                    errmsg = errorResponse.getString("message");
-                } catch (Exception e) {
-
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
-        setupToolbar();
-
-        save = (Button) findViewById(R.id.save_edit);
-
-        nickName_editor= (TextInputEditText) findViewById(R.id.edit_nickname);
-        majorName_editor= (TextInputEditText) findViewById(R.id.edit_major);
-        text_email= (TextInputEditText) findViewById(R.id.edit_email);
-
-        getUserInfo();
-
-
-        save.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setUserInfo();
-            }
-        });
+          nickName_editor.setText(nickName);
+          majorName_editor.setText(majorName);
+          text_email.setText(email);
 
 
 
-    }
 
-    public void setupToolbar(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_edit_profile);
-        toolbar.setTitle(R.string.toolbar_edit_profile);
-        setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setHomeButtonEnabled(true);
-        ab.setDisplayHomeAsUpEnabled(true);
-    }
+        } catch (Exception e) {
+          Log.e("getUserInfo", e.toString());
+        }
+      }
+      @Override
+      public void onFailure(int statusCode, Header[] headers, String errmsg, Throwable throwable) {
+        Log.e("getUserInfo", errmsg);
 
-    /* restore back button functionality*/
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home: {
-                onBackPressed();
-                return true;
-            }
-            default:{return super.onOptionsItemSelected(item);}
+      }
+    });
+  }
+
+  public void setUserInfo() {
+
+    String nickName = this.nickName_editor.getText().toString();
+    String majorName = this.majorName_editor.getText().toString();
+    String email = this.text_email.getText().toString();
+
+    String authToken =  SharedPref.getDefaults("authTokenPython", getApplicationContext());
+
+    final RequestParams params = new RequestParams();
+    params.put("username",nickName);
+    params.put("email",email);
+    params.put("major",majorName);
+    params.setUseJsonStreamer(true);
+
+    UnificiencyClient client = new PythonAPIClient();
+
+    client.addHeader("Authorization", authToken);
+    client.put("users/", params, new JsonHttpResponseHandler() {
+
+      @Override
+      public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        // Pull out the first event on the public timeline
+        try {
+
+          Intent intent = new Intent();
+          intent.putExtra("saveSuccess", "Speicherung erfolgreich");
+          setResult(Activity.RESULT_OK,intent);
+          finish();
+
+        } catch (Exception e) {
+          Log.e("modifyUserInfo", "success");
+        }
+      }
+      @Override
+      public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+        Log.e("modifyUserInfo", errorResponse.toString());
+        String errmsg = null;
+        try {
+          errmsg = errorResponse.getString("message");
+        } catch (Exception e) {
 
         }
+
+        SuperActivityToast.cancelAllSuperToasts();
+        SuperActivityToast.create(getApplicationContext(), new Style(), Style.TYPE_STANDARD)
+            .setText(errmsg)
+            .setDuration(Style.DURATION_LONG)
+            .setFrame(Style.FRAME_KITKAT)
+            .setColor(ResourcesCompat.getColor(getResources(), R.color.red_400, null))
+            .setAnimations(Style.ANIMATIONS_SCALE)
+            .show();
+      }
+    });
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_edit_profile);
+    setupToolbar();
+
+    save = (Button) findViewById(R.id.save_edit);
+
+    nickName_editor= (TextInputEditText) findViewById(R.id.edit_nickname);
+    majorName_editor= (TextInputEditText) findViewById(R.id.edit_major);
+    text_email= (TextInputEditText) findViewById(R.id.edit_email);
+
+    getUserInfo();
+
+
+    save.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        setUserInfo();
+      }
+    });
+
+
+
+  }
+
+  public void setupToolbar(){
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_edit_profile);
+    toolbar.setTitle(R.string.toolbar_edit_profile);
+    setSupportActionBar(toolbar);
+    ActionBar ab = getSupportActionBar();
+    ab.setHomeButtonEnabled(true);
+    ab.setDisplayHomeAsUpEnabled(true);
+  }
+
+  /* restore back button functionality*/
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()){
+      case android.R.id.home: {
+        onBackPressed();
+        return true;
+      }
+      default:{return super.onOptionsItemSelected(item);}
+
     }
+  }
 }
--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/settings/Profile.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/settings/Profile.java
@@ -1,30 +1,27 @@
 package lmu.de.unificiencyandroid.components.settings;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
+import com.github.johnpersano.supertoasts.library.Style;
+import com.github.johnpersano.supertoasts.library.SuperActivityToast;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import cz.msebera.android.httpclient.Header;
 import lmu.de.unificiencyandroid.R;
-import lmu.de.unificiencyandroid.components.groups.Group;
 import lmu.de.unificiencyandroid.network.PythonAPIClient;
 import lmu.de.unificiencyandroid.network.UnificiencyClient;
 import lmu.de.unificiencyandroid.utils.SharedPref;
@@ -32,81 +29,81 @@ import lmu.de.unificiencyandroid.utils.SharedPref;
 public class Profile extends Fragment {
 
 
-    TextView name_Account, email_Account, major_Account;
-    TextView stars_nummber, notes_nummber;
-    RoundImageView roundImageView;
-    FloatingActionButton edit_btn;
+  TextView name_Account, email_Account, major_Account;
+  TextView stars_nummber, notes_nummber;
+  RoundImageView roundImageView;
+  FloatingActionButton edit_btn;
 
- //   ListView listView;
+  //   ListView listView;
 
-    public void getUserInfo() {
-        String authToken =  SharedPref.getDefaults("authTokenPython", getContext());
+  public void getUserInfo() {
+    String authToken =  SharedPref.getDefaults("authTokenPython", getContext());
 
-        final RequestParams params = new RequestParams();
+    final RequestParams params = new RequestParams();
 
-        UnificiencyClient client = new PythonAPIClient();
+    UnificiencyClient client = new PythonAPIClient();
 
-        client.addHeader("Authorization", authToken);
-        client.get("users/", params, new JsonHttpResponseHandler() {
+    client.addHeader("Authorization", authToken);
+    client.get("users/", params, new JsonHttpResponseHandler() {
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                try {
+      @Override
+      public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        try {
 
-                    String nickName = response.getString("username");
-                    String majorName = response.getString("major");
-                    String email = response.getString("email");
+          String nickName = response.getString("username");
+          String majorName = response.getString("major");
+          String email = response.getString("email");
 
-                    name_Account.setText(nickName);
-                    major_Account.setText(majorName);
-                    email_Account.setText(email);
+          name_Account.setText(nickName);
+          major_Account.setText(majorName);
+          email_Account.setText(email);
 
 
-                } catch (Exception e) {
-                    Log.e("getUserInfo", e.toString());
-                }
-            }
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String errmsg, Throwable throwable) {
-                Log.e("getUserInfo", errmsg);
+        } catch (Exception e) {
+          Log.e("getUserInfo", e.toString());
+        }
+      }
+      @Override
+      public void onFailure(int statusCode, Header[] headers, String errmsg, Throwable throwable) {
+        Log.e("getUserInfo", errmsg);
 
-            }
-        });
-    }
+      }
+    });
+  }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view =  inflater.inflate(R.layout.account,null);
+    View view =  inflater.inflate(R.layout.account,null);
 
-        name_Account= (TextView) view.findViewById(R.id.name_account);
-        email_Account= (TextView) view.findViewById(R.id.email_account);
-        major_Account= (TextView) view.findViewById(R.id.major_account);
+    name_Account= (TextView) view.findViewById(R.id.name_account);
+    email_Account= (TextView) view.findViewById(R.id.email_account);
+    major_Account= (TextView) view.findViewById(R.id.major_account);
 
-        stars_nummber=(TextView) view.findViewById(R.id.stars_nummber);
-        notes_nummber=(TextView) view.findViewById(R.id.notes_nummber);
+    stars_nummber=(TextView) view.findViewById(R.id.stars_nummber);
+    notes_nummber=(TextView) view.findViewById(R.id.notes_nummber);
 
-        edit_btn=(FloatingActionButton) view.findViewById(R.id.edit_floating_button);
+    edit_btn=(FloatingActionButton) view.findViewById(R.id.edit_floating_button);
 
-        edit_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent= new Intent(getContext(), EditProfile.class);
-                startActivity(intent);
-            }
-        });
+    edit_btn.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        Intent intent= new Intent(getContext(), EditProfile.class);
+        startActivityForResult(intent, 1);
+      }
+    });
 
-  //      listView=(ListView) view.findViewById(R.id.group_list_account);
-  //       name_Account.setText("ostdong");
-  //       email_Account.setText("ostdong@gmail.com");
-  //      major_Account.setText("informatik");
+    //      listView=(ListView) view.findViewById(R.id.group_list_account);
+    //       name_Account.setText("ostdong");
+    //       email_Account.setText("ostdong@gmail.com");
+    //      major_Account.setText("informatik");
 
-        stars_nummber.setText("18");
-        notes_nummber.setText("6");
+    stars_nummber.setText("18");
+    notes_nummber.setText("6");
 
-        getUserInfo();
+    getUserInfo();
 
-        //fake data
+    //fake data
   /*      ArrayList<String> GroupMember =new ArrayList<String>(){{
             add("Zhenhao");
             add("Robert");
@@ -127,6 +124,38 @@ public class Profile extends Fragment {
         listView.setAdapter(adapter);
 
 */
-        return view;
+    return view;
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    if (requestCode == 1) {
+      if(resultCode == Activity.RESULT_OK){
+
+        Bundle extras = data.getExtras();
+        String registeredMsg;
+
+        getUserInfo();
+
+        if (extras != null) {
+          registeredMsg = extras.getString("saveSuccess");
+          SuperActivityToast.cancelAllSuperToasts();
+          SuperActivityToast.create(getContext(), new Style(), Style.TYPE_STANDARD)
+              .setText(registeredMsg)
+              .setDuration(Style.DURATION_LONG)
+              .setFrame(Style.FRAME_KITKAT)
+              .setColor(ResourcesCompat.getColor(getResources(), R.color.green_400, null))
+              .setAnimations(Style.ANIMATIONS_SCALE)
+              .show();
+
+
+        }
+
+      }
+      if (resultCode == Activity.RESULT_CANCELED) {
+        //Write your code if there's no result
+      }
     }
+  }//onActivityResult
 }
