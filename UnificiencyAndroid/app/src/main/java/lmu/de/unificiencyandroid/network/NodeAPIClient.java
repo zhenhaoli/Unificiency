f317dd1214084232dc8801e922373a8af3e5d558
zhenhaoli <zhenhao.li@outlook.com>
2017-01-23 16:26:12 +0100

    api adjustment

--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/network/NodeAPIClient.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/network/NodeAPIClient.java
@@ -1,8 +1,8 @@
 package lmu.de.unificiencyandroid.network;
 
 public class NodeAPIClient extends UnificiencyClient {
-  //private final String BASE_URL = "http://li.mz-host.de:5048/";
-  private final String BASE_URL = "http://141.84.214.127:5048/";
+  private final String BASE_URL = "http://li.mz-host.de:5048/";
+  //private final String BASE_URL = "http://141.84.214.127:5048/";
   //private final String BASE_URL = "http://192.168.178.24:5048/";
   public NodeAPIClient(){
     super();
