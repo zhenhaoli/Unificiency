8570bb166c94aa9ac1111dba51a71d465738c5eb
zhenhaoli <zhenhao.li@outlook.com>
2017-01-24 22:50:55 +0100

    labeling

--- UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/settings/SettingsTab.java
+++ UnificiencyAndroid/app/src/main/java/lmu/de/unificiencyandroid/components/settings/SettingsTab.java
@@ -62,7 +62,7 @@ public class SettingsTab extends Fragment {
         case 0 :
           return "Profile";
         case 1 :
-          return "Notifications";
+          return "Benachrichtigungen";
       }
       return null;
     }
